f269de8496bb5cde7c993715c434060e42745b47
R3S <ricardo.tercero@r3s.com.mx>
2022-12-27 16:25:36 -0600

    Add xml convert functionality to html page

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -1,10 +1,27 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.core.exceptions import BadRequest
+from xml_converter.converted import convert_to_json
 
 
 def upload_page(request):
     if request.method == 'POST':
-        # TODO: Convert the submitted XML file into a JSON object and return to the user.
-        return JsonResponse({})
+        # Valid the file has been provided
+        if 'file' not in request.FILES or request.FILES['file'] is None:
+            raise BadRequest("No Input File provided")
+
+        # Fetch content of uploaded file
+        file = request.FILES['file']
+        file.open()
+        content = file.read()
+        file.close()
+
+        # Valid is not an empty file
+        if len(content.strip()) == 0:
+            raise BadRequest("Empty File provided")
+
+        json_obj = convert_to_json(content)
+
+        return JsonResponse(json_obj)
 
     return render(request, "upload_page.html")
