7601ea23f45c2975404219078f7f238ae8834fae
R3S <ricardo.tercero@r3s.com.mx>
2022-12-27 18:42:05 -0600

    Create function to parse xml to json

--- xml_converter/api.py
+++ xml_converter/api.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from rest_framework.exceptions import ValidationError
+from xml_converter.converted import convert_to_json
 
 
 class ConverterViewSet(ViewSet):
@@ -11,4 +13,20 @@ class ConverterViewSet(ViewSet):
 
     @action(methods=["POST"], detail=False, url_path="convert")
     def convert(self, request, **kwargs):
-        return Response({})
+        # Valid the file has been provided
+        if 'file' not in request.FILES or request.FILES['file'] is None:
+            raise ValidationError("No Input File provided")
+
+        # Fetch content of uploaded file
+        file = request.FILES['file']
+        file.open()
+        content = file.read()
+        file.close()
+
+        # Valid is not an empty file
+        if len(content.strip()) == 0:
+            raise ValidationError("Empty File provided")
+
+        json_obj = convert_to_json(content)
+
+        return Response(json_obj)
--- xml_converter/converted.py
+++ xml_converter/converted.py
@@ -1,9 +1,41 @@
-import xmltodict
-import json
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+
+def convert_node_to_json(xml_node, is_root=True):
+    node_list = []
+    for item in xml_node:
+        if isinstance(item, Tag):
+            if item.is_empty_element:
+                if is_root:
+                    return {item.name: ""}
+                node_list.append(
+                    {item.name: ""}
+                )
+            elif len(item.findChildren()) == 0:  # is a Leaf Node
+                if is_root:
+                    return {item.name: item.getText()}
+                node_list.append(
+                    {item.name: item.getText()}
+                )
+            else:
+                # the node is a Non Leaf
+                nodes = []
+                for node in item.children:
+                    if isinstance(node, Tag):
+                        nodes.append(
+                            {
+                                node.name: convert_node_to_json(node, is_root=False)
+                            }
+                        )
+
+                return {
+                    item.name: nodes
+                }
+    return node_list
 
 
 def convert_to_json(xml_content):
-    parse_obj = xmltodict.parse(xml_content)
-    json_string = json.dumps(parse_obj)
-
-    return json.loads(json_string)
+    parser = BeautifulSoup(xml_content, "xml")
+    dict_obj = convert_node_to_json(parser)
+    return dict_obj
